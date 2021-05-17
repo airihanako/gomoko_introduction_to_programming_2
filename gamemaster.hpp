53f85bdbb2d216bed53084c420f13fe447cffdab
szoor <ashekogmaw@gmail.com>
2021-05-17 20:43:04 +0200

    gamemaster added

---
+++ gamemaster.hpp
@@ -0,0 +1,13 @@
+#ifndef UNTITLED_GAMEMASTER_HPP
+#define UNTITLED_GAMEMASTER_HPP
+#include "vector"
+
+struct GameMaster{
+    int tableRepresentation[15][15] = {{0}};
+    GameMaster();
+
+    int chechwin();
+
+};
+
+#endif //UNTITLED_GAMEMASTER_HPP
